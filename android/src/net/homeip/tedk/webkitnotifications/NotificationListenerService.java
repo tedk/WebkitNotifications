631994835f3d5dc8a2ca533b98b3de80cf44a710
Ted Krofssik <ted9k@swbell.net>
2012-10-12 00:43:21 -0700

    get rid of that config file, and use android settings instead

--- android/src/net/homeip/tedk/webkitnotifications/NotificationListenerService.java
+++ android/src/net/homeip/tedk/webkitnotifications/NotificationListenerService.java
@@ -21,6 +21,8 @@ import android.accessibilityservice.AccessibilityService;
 import android.app.Notification;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -29,195 +31,243 @@ import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.util.Base64;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
-public class NotificationListenerService extends AccessibilityService {
+public class NotificationListenerService extends AccessibilityService implements
+		OnSharedPreferenceChangeListener {
 
-   /**
-    * Trust every server - dont check for any certificate
-    * Source: http://stackoverflow.com/questions/2893819/telling-java-to-accept-self-signed-ssl-certificate
-    */
-   private static void trustAllHosts() {
-      // Create a trust manager that does not validate certificate chains
-      TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-         public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-            return new java.security.cert.X509Certificate[] {};
-         }
+	/**
+	 * Trust every server - dont check for any certificate Source:
+	 * http://stackoverflow
+	 * .com/questions/2893819/telling-java-to-accept-self-signed-ssl-certificate
+	 */
+	private static void trustAllHosts() {
+		// Create a trust manager that does not validate certificate chains
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return new java.security.cert.X509Certificate[] {};
+			}
 
-         public void checkClientTrusted(X509Certificate[] chain, String authType)
-               throws CertificateException {}
+			public void checkClientTrusted(X509Certificate[] chain,
+					String authType) throws CertificateException {
+			}
 
-         public void checkServerTrusted(X509Certificate[] chain, String authType)
-               throws CertificateException {}
-      } };
+			public void checkServerTrusted(X509Certificate[] chain,
+					String authType) throws CertificateException {
+			}
+		} };
 
-      // Install the all-trusting trust manager
-      try {
-         SSLContext sc = SSLContext.getInstance("TLS");
-         sc.init(null, trustAllCerts, new java.security.SecureRandom());
-         HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
-   static {
-	   trustAllHosts();
-   }
+		// Install the all-trusting trust manager
+		try {
+			SSLContext sc = SSLContext.getInstance("TLS");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection
+					.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-   private DateFormat dateFormat = null;
-   private ConnectivityManager cm = null;
-   private PackageManager pm = null;
+	static {
+		trustAllHosts();
+	}
 
-   private URL serverUrl = null;
-   private String encodedAuthentication = null;
+	private DateFormat dateFormat = null;
+	private ConnectivityManager cm = null;
+	private PackageManager pm = null;
 
-   @Override
-   protected void onServiceConnected() {
-      super.onServiceConnected();
-      dateFormat = DateFormat.getTimeInstance(DateFormat.LONG);
-      pm = getPackageManager();
-      cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	private URL serverUrl = null;
+	private boolean authenticationEnabled = false;
+	private String encodedAuthentication = null;
 
-      try {
-         serverUrl = new URL(getResources().getString(R.string.server_url));
-      } catch (MalformedURLException e) {
-         //Log.e("NotificationListenerService", "Bad server_url", e);
-      }
-      String authString = getResources().getString(R.string.server_username)
-            + ":" + getResources().getString(R.string.server_password);
-      encodedAuthentication = Base64.encodeToString(authString.getBytes(),
-            Base64.DEFAULT);
-   }
+	@Override
+	protected void onServiceConnected() {
+		super.onServiceConnected();
+		setUp();
+	}
 
-   @Override
-   public boolean onUnbind(Intent intent) {
-      dateFormat = null;
-      pm = null;
-      cm = null;
+	public void setUp() {
+		dateFormat = DateFormat.getTimeInstance(DateFormat.LONG);
+		pm = getPackageManager();
+		cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-      serverUrl = null;
-      return super.onUnbind(intent);
-   }
+		SharedPreferences sharedPref = PreferenceManager
+				.getDefaultSharedPreferences(this);
 
-   @Override
-   public void onAccessibilityEvent(AccessibilityEvent event) {
-      if (event == null) return;
-      Notification n = (Notification) event.getParcelableData();
-      if (n == null) return;
+		try {
+			serverUrl = new URL(sharedPref.getString("pref_url", ""));
+		} catch (MalformedURLException e) {
+			Log.e("NotificationListenerService", "Bad server_url", e);
+		}
+		authenticationEnabled = sharedPref.getBoolean("pref_auth", false);
+		if (authenticationEnabled) {
+			String authString = sharedPref.getString("pref_auth_username", "")
+					+ ":" + sharedPref.getString("pref_auth_password", "");
+			encodedAuthentication = Base64.encodeToString(
+					authString.getBytes(), Base64.DEFAULT);
+		}
+	}
+	
+	public void tearDown() {
+		dateFormat = null;
+		pm = null;
+		cm = null;
 
-      String text = n.tickerText == null ? null : n.tickerText.toString();
-      if (text == null) return; // ignore blank notifications (downloads, gps,
-                                // keyboard, etc.)
+		serverUrl = null;
+		authenticationEnabled = false;
+		encodedAuthentication = null;
+	}
 
-      NetworkInfo ni = cm.getActiveNetworkInfo();
-      if (ni == null || !ni.getState().equals(NetworkInfo.State.CONNECTED))
-         return;
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		tearDown();
+		setUp();
+	}
 
-      String time = dateFormat.format(new Date());
-      String packageName = event.getPackageName().toString();
-      String appName = null;
-      try {
-         appName = pm
-               .getApplicationLabel(pm.getApplicationInfo(packageName, 0))
-               .toString();
-      } catch (Exception e) {
-         //Log.e("NotificationListenerService", "Could not load application name", e);
-      }
-      if (appName == null) appName = packageName;
-      String num = Integer.toString(n.number);
-      String icon = null;
-      int iconPadding = 0;
-      ByteArrayOutputStream baos = null;
-      try {
-         Context c = createPackageContext(packageName, 0);
-         baos = new ByteArrayOutputStream();
-         BitmapFactory.Options options = new BitmapFactory.Options();
-         options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-         Bitmap iconBitmap = BitmapFactory.decodeResource(c.getResources(), n.icon, options);
-         Bitmap iconWithBackground = Bitmap.createBitmap(iconBitmap.getWidth(), iconBitmap.getHeight(), iconBitmap.getConfig());
-         Canvas background = new Canvas(iconWithBackground);
-         background.drawColor(Color.BLACK);
-         background.drawBitmap(iconBitmap, 0.f, 0.f, null);
-         iconWithBackground.compress(Bitmap.CompressFormat.PNG, 100, baos);
-         baos.flush();
-         icon = Base64.encodeToString(baos.toByteArray(), Base64.NO_WRAP | Base64.URL_SAFE);
-         while (icon.endsWith("=")) {
-            ++iconPadding;
-            icon = icon.substring(0, icon.length() - 1);
-         }
-      } catch (Exception e) {
-         //Log.e("NotificationListenerService", "Could not load icon", e);
-      } finally {
-         if (baos != null) try {
-            baos.close();
-         } catch (Exception e) {}
-      }
-      new SendNotification().execute(time, appName, text, num, icon,
-            Integer.toString(iconPadding));
-   }
+	@Override
+	public boolean onUnbind(Intent intent) {
+		tearDown();
+		return super.onUnbind(intent);
+	}
 
-   @Override
-   public void onInterrupt() {
-	   // do nothing for now
-   }
+	@Override
+	public void onAccessibilityEvent(AccessibilityEvent event) {
+		if (event == null)
+			return;
+		Notification n = (Notification) event.getParcelableData();
+		if (n == null)
+			return;
 
-   private class SendNotification extends AsyncTask<String, Void, Void> {
+		String text = n.tickerText == null ? null : n.tickerText.toString();
+		if (text == null)
+			return; // ignore blank notifications (downloads, gps,
+					// keyboard, etc.)
 
-      // format: time, app, text, num, icon
-      @Override
-      protected Void doInBackground(String... params) {
-         String time = params[0];
-         String app = params[1];
-         String text = params[2];
-         String num = params[3];
-         String icon = params.length > 4 ? params[4] : null;
-         String iconPadding = params.length > 5 ? params[5] : null;
+		NetworkInfo ni = cm.getActiveNetworkInfo();
+		if (ni == null || !ni.getState().equals(NetworkInfo.State.CONNECTED))
+			return;
 
-         //Log.d("NotificationListenerService", time + ", " + app + ", " + text + ", " + num + ", " + (icon == null ? "null" : icon.length()));
+		String time = dateFormat.format(new Date());
+		String packageName = event.getPackageName().toString();
+		String appName = null;
+		try {
+			appName = pm.getApplicationLabel(
+					pm.getApplicationInfo(packageName, 0)).toString();
+		} catch (Exception e) {
+			Log.e("NotificationListenerService", "Could not load application name", e);
+		}
+		if (appName == null)
+			appName = packageName;
+		String num = Integer.toString(n.number);
+		String icon = null;
+		int iconPadding = 0;
+		ByteArrayOutputStream baos = null;
+		try {
+			Context c = createPackageContext(packageName, 0);
+			baos = new ByteArrayOutputStream();
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+			Bitmap iconBitmap = BitmapFactory.decodeResource(c.getResources(),
+					n.icon, options);
+			Bitmap iconWithBackground = Bitmap.createBitmap(
+					iconBitmap.getWidth(), iconBitmap.getHeight(),
+					iconBitmap.getConfig());
+			Canvas background = new Canvas(iconWithBackground);
+			background.drawColor(Color.BLACK);
+			background.drawBitmap(iconBitmap, 0.f, 0.f, null);
+			iconWithBackground.compress(Bitmap.CompressFormat.PNG, 100, baos);
+			baos.flush();
+			icon = Base64.encodeToString(baos.toByteArray(), Base64.NO_WRAP
+					| Base64.URL_SAFE);
+			while (icon.endsWith("=")) {
+				++iconPadding;
+				icon = icon.substring(0, icon.length() - 1);
+			}
+		} catch (Exception e) {
+			Log.e("NotificationListenerService", "Could not load icon", e);
+		} finally {
+			if (baos != null)
+				try {
+					baos.close();
+				} catch (Exception e) {
+				}
+		}
+		new SendNotification().execute(time, appName, text, num, icon,
+				Integer.toString(iconPadding));
+	}
 
-         OutputStream os = null;
-         OutputStreamWriter wr = null;
-         try {
-            JSONObject j = new JSONObject();
-            j.put("type", "notification");
-            JSONObject n = new JSONObject();
-            n.put("time", time);
-            n.put("app", app);
-            n.put("text", text);
-            n.put("num", num);
-            n.put("icon", icon);
-            n.put("iconPadding", iconPadding);
-            j.put("content", n);
-            String json = j.toString();
+	@Override
+	public void onInterrupt() {
+		// do nothing for now
+	}
 
-            HttpsURLConnection conn = (HttpsURLConnection) serverUrl
-                  .openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
-            conn.setFixedLengthStreamingMode(json.length());
-            conn.setRequestProperty("Content-length",
-                  Integer.toString(json.length()));
-            conn.setRequestProperty("Authorization", "Basic "
-                  + encodedAuthentication);
-            os = conn.getOutputStream();
-            wr = new OutputStreamWriter(os);
-            wr.write(json);
-            wr.flush();
-         } catch (Exception e) {
-            //Log.e("NotificationListenerService", "Network Exception", e);
-         } finally {
-            if (wr != null) try {
-               wr.close();
-            } catch (Exception e) {}
-            if (os != null) try {
-               os.close();
-            } catch (Exception e) {}
-         }
+	private class SendNotification extends AsyncTask<String, Void, Void> {
 
-         return null;
-      }
+		// format: time, app, text, num, icon
+		@Override
+		protected Void doInBackground(String... params) {
+			String time = params[0];
+			String app = params[1];
+			String text = params[2];
+			String num = params[3];
+			String icon = params.length > 4 ? params[4] : null;
+			String iconPadding = params.length > 5 ? params[5] : null;
 
-   }
+			Log.d("NotificationListenerService", time + ", " + app + ", " +
+			   text + ", " + num + ", " + (icon == null ? "null" :
+			   icon.length()));
+
+			OutputStream os = null;
+			OutputStreamWriter wr = null;
+			try {
+				JSONObject j = new JSONObject();
+				j.put("type", "notification");
+				JSONObject n = new JSONObject();
+				n.put("time", time);
+				n.put("app", app);
+				n.put("text", text);
+				n.put("num", num);
+				n.put("icon", icon);
+				n.put("iconPadding", iconPadding);
+				j.put("content", n);
+				String json = j.toString();
+
+				HttpsURLConnection conn = (HttpsURLConnection) serverUrl
+						.openConnection();
+				conn.setDoOutput(true);
+				conn.setRequestMethod("POST");
+				conn.setFixedLengthStreamingMode(json.length());
+				conn.setRequestProperty("Content-length",
+						Integer.toString(json.length()));
+				if (authenticationEnabled) {
+					conn.setRequestProperty("Authorization", "Basic "
+							+ encodedAuthentication);
+				}
+				os = conn.getOutputStream();
+				wr = new OutputStreamWriter(os);
+				wr.write(json);
+				wr.flush();
+			} catch (Exception e) {
+				Log.e("NotificationListenerService", "Network Exception", e);
+			} finally {
+				if (wr != null)
+					try {
+						wr.close();
+					} catch (Exception e) {
+					}
+				if (os != null)
+					try {
+						os.close();
+					} catch (Exception e) {
+					}
+			}
+
+			return null;
+		}
+
+	}
 
 }
--- android/src/net/homeip/tedk/webkitnotifications/SettingsActivity.java
+++ android/src/net/homeip/tedk/webkitnotifications/SettingsActivity.java
@@ -1,14 +1,46 @@
 package net.homeip.tedk.webkitnotifications;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings_config);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean authPref = sharedPref.getBoolean("pref_auth", false);
+        findPreference("pref_auth_username").setEnabled(authPref);
+        findPreference("pref_auth_password").setEnabled(authPref);
 	}
+	
+	public static final String KEY_PREF_AUTH = "pref_auth";
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		if (key.equals(KEY_PREF_AUTH)) {
+            boolean authPref = sharedPreferences.getBoolean(key, false);
+            findPreference("pref_auth_username").setEnabled(authPref);
+            findPreference("pref_auth_password").setEnabled(authPref);
+        }
+	}
+	
+	@Override
+	protected void onResume() {
+	    super.onResume();
+	    getPreferenceScreen().getSharedPreferences()
+	            .registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	protected void onPause() {
+	    super.onPause();
+	    getPreferenceScreen().getSharedPreferences()
+	            .unregisterOnSharedPreferenceChangeListener(this);
+	}
+
 	
 }
