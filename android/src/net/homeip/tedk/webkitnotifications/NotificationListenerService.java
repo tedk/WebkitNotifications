1800d4bc20fdebcdf8a31998d46485ab0dd48e53
Ted Krofssik <ted9k@swbell.net>
2012-12-14 18:20:42 -0700

    now plays notification sound

--- android/src/net/homeip/tedk/webkitnotifications/NotificationListenerService.java
+++ android/src/net/homeip/tedk/webkitnotifications/NotificationListenerService.java
@@ -1,6 +1,7 @@
 package net.homeip.tedk.webkitnotifications;
 
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
@@ -19,17 +20,21 @@ import org.json.JSONObject;
 
 import android.accessibilityservice.AccessibilityService;
 import android.app.Notification;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.media.RingtoneManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.util.Base64;
@@ -39,9 +44,8 @@ public class NotificationListenerService extends AccessibilityService implements
 		OnSharedPreferenceChangeListener {
 
 	/**
-	 * Trust every server - dont check for any certificate Source:
-	 * http://stackoverflow
-	 * .com/questions/2893819/telling-java-to-accept-self-signed-ssl-certificate
+	 * Trust every server - don't check for any certificate
+	 * Source: http://stackoverflow.com/questions/2893819/telling-java-to-accept-self-signed-ssl-certificate
 	 */
 	private static void trustAllHosts() {
 		// Create a trust manager that does not validate certificate chains
@@ -51,11 +55,11 @@ public class NotificationListenerService extends AccessibilityService implements
 			}
 
 			public void checkClientTrusted(X509Certificate[] chain,
-					String authType) throws CertificateException {
+				String authType) throws CertificateException {
 			}
 
 			public void checkServerTrusted(X509Certificate[] chain,
-					String authType) throws CertificateException {
+				String authType) throws CertificateException {
 			}
 		} };
 
@@ -63,10 +67,9 @@ public class NotificationListenerService extends AccessibilityService implements
 		try {
 			SSLContext sc = SSLContext.getInstance("TLS");
 			sc.init(null, trustAllCerts, new java.security.SecureRandom());
-			HttpsURLConnection
-					.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
 		} catch (Exception e) {
-			e.printStackTrace();
+		   //Log.e("NotificationListenerService", "Unable to install trust manager", e);
 		}
 	}
 
@@ -93,8 +96,7 @@ public class NotificationListenerService extends AccessibilityService implements
 		pm = getPackageManager();
 		cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-		SharedPreferences sharedPref = PreferenceManager
-				.getDefaultSharedPreferences(this);
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 
 		try {
 			serverUrl = new URL(sharedPref.getString("pref_url", ""));
@@ -103,10 +105,8 @@ public class NotificationListenerService extends AccessibilityService implements
 		}
 		authenticationEnabled = sharedPref.getBoolean("pref_auth", false);
 		if (authenticationEnabled) {
-			String authString = sharedPref.getString("pref_auth_username", "")
-					+ ":" + sharedPref.getString("pref_auth_password", "");
-			encodedAuthentication = Base64.encodeToString(
-					authString.getBytes(), Base64.DEFAULT);
+			String authString = sharedPref.getString("pref_auth_username", "") + ":" + sharedPref.getString("pref_auth_password", "");
+			encodedAuthentication = Base64.encodeToString(authString.getBytes(), Base64.DEFAULT);
 		}
 	}
 	
@@ -120,8 +120,7 @@ public class NotificationListenerService extends AccessibilityService implements
 		encodedAuthentication = null;
 	}
 
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-			String key) {
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 		tearDown();
 		setUp();
 	}
@@ -142,8 +141,7 @@ public class NotificationListenerService extends AccessibilityService implements
 
 		String text = n.tickerText == null ? null : n.tickerText.toString();
 		if (text == null)
-			return; // ignore blank notifications (downloads, gps,
-					// keyboard, etc.)
+			return; // ignore blank notifications (downloads, gps, keyboard, etc.)
 
 		NetworkInfo ni = cm.getActiveNetworkInfo();
 		if (ni == null || !ni.getState().equals(NetworkInfo.State.CONNECTED))
@@ -153,38 +151,33 @@ public class NotificationListenerService extends AccessibilityService implements
 		String packageName = event.getPackageName().toString();
 		String appName = null;
 		try {
-			appName = pm.getApplicationLabel(
-					pm.getApplicationInfo(packageName, 0)).toString();
+			appName = pm.getApplicationLabel(pm.getApplicationInfo(packageName, 0)).toString();
 		} catch (Exception e) {
 			//Log.e("NotificationListenerService", "Could not load application name", e);
 		}
 		if (appName == null)
 			appName = packageName;
 		String num = Integer.toString(n.number);
+		Context c = getApplicationContext();
+		try {
+         c = createPackageContext(packageName, 0);
+      } catch (NameNotFoundException e) {
+         //Log.e("NotificationListenerService", "Could not load application context", e);
+      }
 		String icon = null;
-		int iconPadding = 0;
 		ByteArrayOutputStream baos = null;
 		try {
-			Context c = createPackageContext(packageName, 0);
 			baos = new ByteArrayOutputStream();
 			BitmapFactory.Options options = new BitmapFactory.Options();
 			options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-			Bitmap iconBitmap = BitmapFactory.decodeResource(c.getResources(),
-					n.icon, options);
-			Bitmap iconWithBackground = Bitmap.createBitmap(
-					iconBitmap.getWidth(), iconBitmap.getHeight(),
-					iconBitmap.getConfig());
+			Bitmap iconBitmap = BitmapFactory.decodeResource(c.getResources(), n.icon, options);
+			Bitmap iconWithBackground = Bitmap.createBitmap(iconBitmap.getWidth(), iconBitmap.getHeight(), iconBitmap.getConfig());
 			Canvas background = new Canvas(iconWithBackground);
 			background.drawColor(Color.BLACK);
 			background.drawBitmap(iconBitmap, 0.f, 0.f, null);
 			iconWithBackground.compress(Bitmap.CompressFormat.PNG, 100, baos);
 			baos.flush();
-			icon = Base64.encodeToString(baos.toByteArray(), Base64.NO_WRAP
-					| Base64.URL_SAFE);
-			while (icon.endsWith("=")) {
-				++iconPadding;
-				icon = icon.substring(0, icon.length() - 1);
-			}
+			icon = "data:image/png;base64," + Base64.encodeToString(baos.toByteArray(), Base64.NO_WRAP | Base64.URL_SAFE);
 		} catch (Exception e) {
 			//Log.e("NotificationListenerService", "Could not load icon", e);
 		} finally {
@@ -194,8 +187,43 @@ public class NotificationListenerService extends AccessibilityService implements
 				} catch (Exception e) {
 				}
 		}
-		new SendNotification().execute(time, appName, text, num, icon,
-				Integer.toString(iconPadding));
+		Uri soundUri = null;
+		if(n.sound != null) {
+		   soundUri = n.sound;
+		} else if((n.defaults & Notification.DEFAULT_SOUND) == Notification.DEFAULT_SOUND) {
+		   soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+		}
+		String sound = null;
+		if(soundUri != null) {
+		   ContentResolver cr = c.getContentResolver();
+		   InputStream is = null;
+		   ByteArrayOutputStream buffer = null;
+		   try {
+            is = cr.openInputStream(soundUri);
+            buffer = new ByteArrayOutputStream();
+            int nRead;
+            byte[] data = new byte[1024];
+            while((nRead = is.read(data, 0, data.length)) != -1) {
+               buffer.write(data, 0, nRead);
+            }
+            buffer.flush();
+            sound = "data:audio/mpeg;base64," + Base64.encodeToString(buffer.toByteArray(), Base64.NO_WRAP | Base64.URL_SAFE);
+         } catch (Exception e) {
+            //Log.e("NotificationListenerService", "Could not load sound", e);
+         } finally {
+            if(buffer != null)
+               try {
+                  buffer.close();
+               } catch (Exception e) {
+               }
+            if(is != null)
+               try {
+                  is.close();
+               } catch (Exception e) {
+               }
+         }
+		}
+		new SendNotification().execute(time, appName, text, num, icon, sound);
 	}
 
 	@Override
@@ -205,7 +233,7 @@ public class NotificationListenerService extends AccessibilityService implements
 
 	private class SendNotification extends AsyncTask<String, Void, Void> {
 
-		// format: time, app, text, num, icon
+		// format: time, app, text, num, icon, sound, soundType
 		@Override
 		protected Void doInBackground(String... params) {
 			String time = params[0];
@@ -213,9 +241,9 @@ public class NotificationListenerService extends AccessibilityService implements
 			String text = params[2];
 			String num = params[3];
 			String icon = params.length > 4 ? params[4] : null;
-			String iconPadding = params.length > 5 ? params[5] : null;
+			String sound = params.length > 5 ? params[5] : null;
 
-			//Log.d("NotificationListenerService", time + ", " + app + ", " + text + ", " + num + ", " + (icon == null ? "null" : icon.length()));
+			//Log.d("NotificationListenerService", time + ", " + app + ", " + text + ", " + num + ", " + (icon == null ? "null" : icon.length()) + ", " + (sound == null ? "null" : sound.length()));
 
 			OutputStream os = null;
 			OutputStreamWriter wr = null;
@@ -228,20 +256,17 @@ public class NotificationListenerService extends AccessibilityService implements
 				n.put("text", text);
 				n.put("num", num);
 				n.put("icon", icon);
-				n.put("iconPadding", iconPadding);
+				n.put("sound", sound);
 				j.put("content", n);
 				String json = j.toString();
 
-				HttpsURLConnection conn = (HttpsURLConnection) serverUrl
-						.openConnection();
+				HttpsURLConnection conn = (HttpsURLConnection) serverUrl.openConnection();
 				conn.setDoOutput(true);
 				conn.setRequestMethod("POST");
 				conn.setFixedLengthStreamingMode(json.length());
-				conn.setRequestProperty("Content-length",
-						Integer.toString(json.length()));
+				conn.setRequestProperty("Content-length", Integer.toString(json.length()));
 				if (authenticationEnabled) {
-					conn.setRequestProperty("Authorization", "Basic "
-							+ encodedAuthentication);
+					conn.setRequestProperty("Authorization", "Basic " + encodedAuthentication);
 				}
 				os = conn.getOutputStream();
 				wr = new OutputStreamWriter(os);
