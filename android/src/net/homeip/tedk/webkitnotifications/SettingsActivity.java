984df60f3381276e41ffb9db58522105ee3e4744
tedk <ted9k@swbell.net>
2013-02-05 19:27:28 -0700

    fix deprecated methods

--- android/src/net/homeip/tedk/webkitnotifications/SettingsActivity.java
+++ android/src/net/homeip/tedk/webkitnotifications/SettingsActivity.java
@@ -4,18 +4,33 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+	
+	public static class SettingsFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.settings_config);
+		    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+		    boolean authPref = sharedPref.getBoolean("pref_auth", false);
+		    findPreference("pref_auth_username").setEnabled(authPref);
+		    findPreference("pref_auth_password").setEnabled(authPref);
+		}
+	}
 
+	private SettingsFragment sf;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.settings_config);
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean authPref = sharedPref.getBoolean("pref_auth", false);
-        findPreference("pref_auth_username").setEnabled(authPref);
-        findPreference("pref_auth_password").setEnabled(authPref);
+        
+		sf = new SettingsFragment();
+		getFragmentManager().beginTransaction().replace(android.R.id.content, sf).commit();
+		
+ 
 	}
 	
 	public static final String KEY_PREF_AUTH = "pref_auth";
@@ -23,22 +38,22 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 			String key) {
 		if (key.equals(KEY_PREF_AUTH)) {
             boolean authPref = sharedPreferences.getBoolean(key, false);
-            findPreference("pref_auth_username").setEnabled(authPref);
-            findPreference("pref_auth_password").setEnabled(authPref);
+            sf.findPreference("pref_auth_username").setEnabled(authPref);
+            sf.findPreference("pref_auth_password").setEnabled(authPref);
         }
 	}
 	
 	@Override
 	protected void onResume() {
 	    super.onResume();
-	    getPreferenceScreen().getSharedPreferences()
+	    sf.getPreferenceScreen().getSharedPreferences()
 	            .registerOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
 	protected void onPause() {
 	    super.onPause();
-	    getPreferenceScreen().getSharedPreferences()
+	    sf.getPreferenceScreen().getSharedPreferences()
 	            .unregisterOnSharedPreferenceChangeListener(this);
 	}
 
